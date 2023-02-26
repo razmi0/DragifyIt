abe46f790e705365b2c4cec405f051ba621275d0
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-02-26 16:31:35 +0100

    indentation

--- assets/theme.js
+++ assets/theme.js
@@ -1,9 +1,12 @@
 export function changeTheme() {
   document.querySelector("#theme").checked ? darkTheme() : lightTheme();
+  
 }
 function darkTheme() {
-  document.documentElement.style.setProperty("--bg-color", "#212121");
+  document.documentElement.style.setProperty("--bg-color" , "linear-gradient( 135deg, #F05F57 10%, #360940 100%)");
+  // document.nav.style.setProperty("--bg-color-nav-dark" , "--bg-color-nav-light");
 }
 function lightTheme() {
-  document.documentElement.style.setProperty("--bg-color", "#e8e8e8");
+  document.documentElement.style.setProperty("--bg-color" , "linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)");
+  // document.nav.style.setProperty("--bg-color-nav-light" , "--bg-color-nav-dark");
 }
